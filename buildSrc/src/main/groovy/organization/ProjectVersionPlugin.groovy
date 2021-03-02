95cf621add9bbe6df3eaae0ee79a2685a3e6f84e
Chin Huang <pukkaone@gmail.com>
2021-03-02 12:51:57 -0800

    Enable build when source is not in a Git working tree

--- buildSrc/src/main/groovy/organization/ProjectVersionPlugin.groovy
+++ buildSrc/src/main/groovy/organization/ProjectVersionPlugin.groovy
@@ -1,9 +1,7 @@
 package organization
 
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.api.Status
 import org.eclipse.jgit.api.errors.GitAPIException
-import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -17,23 +15,20 @@ class ProjectVersionPlugin implements Plugin<Project> {
 
   private static final Logger LOGGER = Logging.getLogger(ProjectVersionPlugin)
 
-  private static Git open(Project project) {
-    Repository repository = new FileRepositoryBuilder()
-        .readEnvironment()
-        .findGitDir(project.projectDir)
-        .build()
-    return new Git(repository)
-  }
-
   private static String readVersion(Project project) {
     String version = 'UNKNOWN'
     try {
-      open(project).withCloseable { git ->
-        Status status = git.status().call()
-        version = git.describe().call()
-        if (!status.clean || version ==~ /.*-\d+-g[0-9a-f]+/) {
-          // Assign version for work in progress.
-          version = '999-SNAPSHOT'
+      def repositoryBuilder = new FileRepositoryBuilder()
+          .readEnvironment()
+          .findGitDir(project.projectDir)
+      if (repositoryBuilder.gitDir != null) {
+        new Git(repositoryBuilder.build()).withCloseable { git ->
+          def status = git.status().call()
+          version = git.describe().call()
+          if (!status.clean || version ==~ /.*-\d+-g[0-9a-f]+/) {
+            // Assign version for work in progress.
+            version = '999-SNAPSHOT'
+          }
         }
       }
     } catch (GitAPIException | IOException e) {
